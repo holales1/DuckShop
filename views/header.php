92390ba963848a1861a76e2b25ba1d1548f41b2d
NikosKarakatsidis <nikosxfx@hotmail.com>
2020-03-16 09:44:30 +0100

    testing

---
+++ views/header.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="utf-8">
+	<title>Header</title>
+    <link rel="stylesheet" href="../style.css">
+</head>
+<body>
+	<header id="main-header">
+		<div id="logo-header" href="#">
+			<h2 class="site-name">Duck Shop</h2>
+		</div> <!-- / #logo-header -->
+		<nav>
+			<ul>
+				<li><a href="#">Home</a></li>
+				<li><a href="#">About Us</a></li>
+				<li><a href="#">Contact</a></li>
+			</ul>
+		</nav><!-- / nav -->
+	</header><!-- / #main-header -->
+</body>
+</html>
