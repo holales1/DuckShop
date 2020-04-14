c7a07371061507ab3ccadeb9cfee7fd39fcb78b1
holales1 <alejandrovaltor.x2@gmail.com>
2020-04-14 21:37:32 +0200

    Delete product almmost done

--- views/deleteProduct.php
+++ views/deleteProduct.php
@@ -6,10 +6,11 @@ require_once('../db/functions.php');
 $db_handle = new DBController();
 $ProductID=$_GET['ProductID'];
 $product = $db_handle->selectLogin("SELECT * FROM products WHERE ProductID='$ProductID'");
-if($product[0]['isAvaliable']==0){
-    $db_handle->insertRow("UPDATE products SET isAvaliable=1 WHERE ProductID='$ProductID");
+$fProduct=mysqli_fetch_array($product);
+if($fProduct['isAvaliable']=="0"){
+    $db_handle->insertRow("UPDATE products SET isAvaliable='1' WHERE ProductID='$ProductID'");
 }else{
-    $db_handle->insertRow("UPDATE products SET isAvaliable=0 WHERE ProductID='$ProductID");
+    $db_handle->insertRow("UPDATE products SET isAvaliable='0' WHERE ProductID='$ProductID'");
 }
 
 $_SESSION['message'] = "Product deleted.";
