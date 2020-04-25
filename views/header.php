0f53de6bba68a11203cb4e1a6467aabd3a835ef5
holales1 <alejandrovaltor.x2@gmail.com>
2020-04-25 13:44:30 +0200

    css change

--- views/header.php
+++ views/header.php
@@ -10,7 +10,7 @@ if(session_status() == PHP_SESSION_NONE)
 	<meta charset="utf-8">
 	<title>Header</title>
 	<link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
-	<link rel="stylesheet" href="../style.css">
+	<link rel="stylesheet" href="css/style.css">
 	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.0/jquery.min.js"></script>
   	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" />
   	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
